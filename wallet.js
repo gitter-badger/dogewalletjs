15bb317708fce52202a8cf8aa90ac824749f2e3e
Bradley Matusiak <bmatusiak@gmail.com>
2014-02-20 17:47:28 -0500

    update

--- wallet.js
+++ wallet.js
@@ -18,3 +18,12 @@ console.log("SIGN",sig);
 var verify = Bitcoin.Message.verifyMessage(wallet.address,sig,message);
 
 console.log("VERIFY",verify);
+
+/* OUTPUT OF THIS FILE SHOULD LOOK LIKE THIS (with different data) but "VERIFY true" shoult be the same
+
+{ address: 'DGid2aPnJfr6nTATWwrSgeYxXoAmbDNVZP',
+  privateKey: '5K4VTAMb94fWWZiMfVWyfAWZ4FVAG4essfd3D3xQxuP7vosqLqm' }
+SIGN GxbwvVRnfQp3tw6puBr1EXxom/R/W8EHU+P1rE9ds74SnpisA0aXU6q2jnD5GH9csQYzQDol8suMZF9FgngJptw=
+VERIFY true
+
+*/
